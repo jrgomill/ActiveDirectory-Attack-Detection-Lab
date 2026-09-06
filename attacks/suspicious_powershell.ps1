32a157d461c950651462b4de14b3257cc01d0e41
jrgomill <110260481+jrgomill@users.noreply.github.com>
2026-09-06 15:30:35 -0400

    Update suspicious_powershell.ps1

--- attacks/suspicious_powershell.ps1
+++ attacks/suspicious_powershell.ps1
@@ -1,1 +1,10 @@
+# suspicious_powershell.ps1
+# Example of encoded PowerShell command to trigger Event ID 4104.
+
+$command = 'Write-Host "This is a test of suspicious PowerShell activity"'
+$bytes   = [System.Text.Encoding]::Unicode.GetBytes($command)
+$encoded = [Convert]::ToBase64String($bytes)
+
+Write-Host "[*] Running encoded PowerShell command..."
+powershell.exe -EncodedCommand $encoded
 
